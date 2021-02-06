d14d3084f062c8398755a137125c83719cbe1826
abrarnaim015 <aabrarnaim24@gmail.com>
2021-02-06 15:34:33 +0700

    fix bug and error

--- client/src/components/Loading-comp.jsx
+++ client/src/components/Loading-comp.jsx
@@ -5,7 +5,7 @@ export default function Loading () {
     <>
       <div className="d-flex justify-content-center w3-animate-opacity" style={{height: '100vh'}}>
       <div className="align-self-center">
-        <div className="spinner-grow text-danger" role="status" style={{width: '5em', height: '5em'}}>
+        <div className="spinner-grow" role="status" style={{width: '5em', height: '5em', color: '#33BDBE'}}>
         </div>
         <div className="text-center textLoading" style={{ marginLeft: '-30px'}}><h1>Loading...</h1></div>
       </div>
